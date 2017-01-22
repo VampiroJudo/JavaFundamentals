2c41bb435af5e66f6f4bb9c25e14dcf531393cf7
John <johnconley507@hotmail.com>
2017-01-22 18:46:06 -0500

    working on variable structures

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,17 @@
+package greetings.hello;
+
 public class Helloworld
 {
     public static void main(String[] arg)
     {
         System.out.println("Hello World!");
+
+        int number = 3;
+
+        String name = "Joe";
+
+        System.out.println(number);
+        System.out.println(name);
+
     }
 }

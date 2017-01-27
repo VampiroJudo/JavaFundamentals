39bb407816182b5e7eac7714412c1518fbafd955
John <johnconley507@hotmail.com>
2017-01-27 14:56:29 -0500

    working with arrayrays

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,18 +5,27 @@ public class Helloworld {
     public static void main(String[] arg) {
         System.out.println("Hello World!");
 
-        int number = 3;
+//        int number = 3;
+//
+//        String firstName = "John";
+//        String fullName = firstName + " Conley"
+//        
+//        number = 5 + 2;
+//        
+//        
+//        
+//        System.out.println(number);
+//        System.out.println(full Name);
+//        System.out.println(fullName.length());
+//        System.out.println(fullName.indexOf('y'));
 
-        String firstName = "John";
-        String fullName = firstName + " Conley"
+//Working with arrays
         
-        number = 5 + 2;
+        int[] numbers = new int[10];
         
+        numbers[0] = 5;
+        numbers[1] = 8;
         
-        
-        System.out.println(number);
-        System.out.println(full Name);
-        System.out.println(fullName.indexOf("y"))
-
+        System.out.println(numbers[1]);
     }
 }

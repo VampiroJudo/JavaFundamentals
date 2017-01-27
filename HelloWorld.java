63068eea6656c9021cd81fd924ab7d12fda88494
John <johnconley507@hotmail.com>
2017-01-27 14:49:45 -0500

    practicing string configs

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,23 +1,22 @@
 package greetings.hello;
 
-public class Helloworld
-{
-    public static void main(String[] arg)
-    {
+public class Helloworld {
+	
+    public static void main(String[] arg) {
         System.out.println("Hello World!");
 
         int number = 3;
 
-        String firstName = "Joe";
-        
-        firstName = "Jimmy";
+        String firstName = "John";
+        String fullName = firstName + " Conley"
         
         number = 5 + 2;
         
         
         
         System.out.println(number);
-        System.out.println(firstName);
+        System.out.println(full Name);
+        System.out.println(fullName.indexOf("y"))
 
     }
 }
